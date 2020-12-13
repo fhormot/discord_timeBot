7b4122c8b54e57ddf3dab05ac03fc671987fa9bb
Filip Hormot <f.hormot@gmail.com>
2020-12-13 13:24:15 +0100

    Cleanup, token set as env

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
+require('dotenv').config();
+
 const Discord = require("discord.js");
-const fs = require("fs"); 
+const axios = require('axios').default;
+const axios_url = 'http://api.timezonedb.com/v2.1/get-time-zone';
 
 const client = new Discord.Client(); 
-const token = fs.readFileSync("token.txt").toString(); 
+
+client.login(process.env.DISCORD_TOKEN);
 
 client.once("ready", () => { 
 	console.log("Ready!");
 });
 
 client.on("message", message => {
-    if(message.content === '?time'){
-        console.log(msg)
-    }
+    if(message.content[0] === '?'){
+        const command = message.content.split(' ')[0].substr(1);
+        if (commands.has(command)) { // checks if the map contains the command
+            commands.get(command)(message) // runs the command
+        }
+    }  
 });
 
-client.login(token);
+function extractArgument(msg) {
+    return msg.content.split(' ')[1];
+}
+    
+let commands = new Map();
+
+commands.set("time", (msg) => {
+    // msg.channel.send(extractArgument(msg));
+    const requestCity = extractArgument(msg);
+
+    axios.get(`${axios_url}/geo/cities/68526/time`)
+        .then((response) => {
+            // msg.channel.send(response);
+            console.log(response);
+        })
+        .catch((response) => {
+            // msg.channel.send(response);
+            console.log(response);
+        })
+})
