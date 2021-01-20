99cae7dfb1ced90812a05e38a0c5ba83f48c6e7d
Filip Hormot <f.hormot@gmail.com>
2021-01-20 22:07:15 +0100

    moved helper functions in a separate file. Help is now in an embed

--- components/commands.js
+++ components/commands.js
@@ -5,37 +5,26 @@ const time_API_url = `https://api.ipgeolocation.io/timezone?apiKey=${process.env
 
 const giphy = require('giphy')(process.env.GIPHY_API_KEY);
 
-let commands = new Map();
+let commands = new Map();               // Map of commands
+const resp_num = 20;                    // Giphy result limiter
 
-function extractArgument(msg) {
-    const str = msg.content.split(' ').slice(1).join(" ");
-
-    return (str) ? str : "";
-}
-
-function msgEmbed(msg, gif) {
-    return new Discord.MessageEmbed()
-        .setDescription(`${msg}`)
-        .setImage(gif);
-    // return { embed: {
-    //     color: 0x0099ff,
-    //     description: `${msg}`,
-    //     image: {
-    //         url: `${gif}`
-    //     }
-    // }}
-}
+const {
+    extractArgument,
+    map2list,
+    msgEmbed,
+} = require('./helper-functions');
 
 commands.set("help", (msg) => {
-    msg.channel.send(
-        `Use '?' to address the bot. The following commands are available:
-        \t- help
-            \t\t prints this help dialog.
-        \t- time (city)
-            \t\t Returns the current time in the given city (eg. ?time Tokyo will return the current time in Tokyo).
-        \t- gj
-            \t\t Congratulate yourself on a good job. Or mention somebody else.`
-    );
+    const resp_help = new Discord.MessageEmbed()
+        .setTitle('Let me help you better use me!')
+        .setDescription('Use \'?\' to address me. The following commands are available:')
+        .addFields(
+            { name: 'help', value: 'Prints this help dialog' },
+            { name: 'time (city)', value: 'Returns the current time in the given city (eg. ?time Tokyo will return the current time in Tokyo)' },
+            { name: 'gj (mention)', value: 'Congratulate yourself on a good job. Or mention somebody else.' }
+        );
+
+    msg.channel.send(resp_help);
 });
 
 commands.set("time", (msg) => {
@@ -59,20 +48,16 @@ commands.set("time", (msg) => {
 });
 
 commands.set("gj", (msg) => {
-    const args = extractArgument(msg);
-    const resp_num = 20;
+    const args = extractArgument(msg);      // Extract arguments from the message
 
     giphy.search({q: "Good Job", limit: resp_num}, (err, resp) => {
         if(!err){
             const author = msg.author;
-            const mentionMap = msg.mentions.users;
 
-            let mentionList = [];
+            // console.log(msg);
+            let mentionList = map2list(msg.mentions.users);
 
-            for (var entry of mentionMap.entries()){
-                mentionList.push(entry[1]);
-            }
-
+            // Pull of a gif
             const resp_gif = resp.data[Math.floor(Math.random()*resp_num)].images.original.url;
 
             let resp_msg = `${author} wants to say good job to`;
